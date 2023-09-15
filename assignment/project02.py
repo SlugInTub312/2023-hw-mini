f42a81af2ccc2bc65b115cdef0e1394bfbd1cde8
SlugInTub312 <144175012+SlugInTub312@users.noreply.github.com>
2023-09-15 16:24:34 -0400

    Update project02.py

--- assignment/project02.py
+++ assignment/project02.py
@@ -7,6 +7,7 @@ use two simultaneously running threads to:
 
 import machine
 import time
+import random
 import _thread
 import json
 import os
@@ -45,6 +46,79 @@ def is_regular_file(path: str) -> bool:
     except OSError:
         return False
 
+def random_time_interval(tmin: float, tmax: float) -> float:
+    """return a random time interval between max and min"""
+    return random.uniform(tmin, tmax)
+
+def write_json(json_filename: str, data: dict) -> None:
+    """Writes data to a JSON file.
+
+    Parameters
+    ----------
+
+    json_filename: str
+        The name of the file to write to. This will overwrite any existing file.
+
+    data: dict
+        Dictionary data to write to the file.
+    """
+
+    with open(json_filename, "a") as f:
+        json.dump(data, f)
+        
+def blinker(N: int, led: Pin) -> None:
+    # %% let user know game started / is over
+
+    for _ in range(N):
+        led.high()
+        time.sleep(0.1)
+        led.low()
+        time.sleep(0.1)
+        
+def scorer(t: list[int | None]) -> None:
+    # %% collate results
+    misses = t.count(None)
+    total_flashes = len(t)
+    non_misses = total_flashes - misses
+    
+    print(f"You missed the light {misses} / {len(t)} times")
+
+    t_good = [x for x in t if x is not None]
+
+    print(t_good)
+
+    # add key, value to this dict to store the minimum, maximum, average response time
+    # and score (non-misses / total flashes) i.e. the score a floating point number
+    # is in range [0..1]
+    if t_good:
+        min_response_time = min(t_good)
+        max_response_time = max(t_good)
+        average_response_time = sum(t_good) / len(t_good)
+        score = non_misses / total_flashes
+    else:
+        min_response_time = 0
+        max_response_time = 0
+        average_response_time = 0
+        score = 0.0
+    
+    # dictionary to store the results
+    data = {
+        "min_response_time": min_response_time,
+        "max_response_time": max_response_time,
+        "average_response_time": average_response_time,
+        "score": score,
+    }
+
+    # %% make dynamic filename and write JSON
+
+    now: tuple[int] = time.localtime()
+
+    now_str = "-".join(map(str, now[:3])) + "T" + "_".join(map(str, now[3:6]))
+    filename = f"proj2-{now_str}.json"
+
+    print("write", filename)
+
+    write_json(filename, data)
 
 def photocell_logger(N: int, sample_interval_s: float) -> None:
     """
@@ -85,7 +159,7 @@ def photocell_logger(N: int, sample_interval_s: float) -> None:
 
     print("light measurement done: write", filename)
 
-    project01.write_json(filename, data)
+    write_json(filename, data)
 
 
 def blinker_response_game(N: int) -> None:
@@ -99,10 +173,10 @@ def blinker_response_game(N: int) -> None:
     t1: list[float | None] = []
     t2: list[float | None] = []
 
-    project01.blinker(3, led)
+    blinker(3, led)
 
     for i in range(N):
-        time.sleep(project01.random_time_interval(0.5, 5.0))
+        time.sleep(random_time_interval(0.5, 5.0))
 
         led.high()
 
@@ -121,10 +195,10 @@ def blinker_response_game(N: int) -> None:
 
         led.low()
 
-    project01.blinker(5, led)
+    blinker(5, led)
 
-    project01.scorer(t1)
-    project01.scorer(t2)
+    scorer(t1)
+    scorer(t2)
 
 
 _thread.start_new_thread(photocell_logger, (10, 0.5))
